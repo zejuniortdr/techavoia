064b13b9112e0e817d2f0637de49b7bf55ee53b7
Jose Luis da Cruz Junior <zejuniortdr@gmail.com>
2024-05-11 01:06:09 -0300

    fix allowed hosts

--- conf/settings.py
+++ conf/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY", default="foobar")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
